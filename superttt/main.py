b35c24c7ce8e71d736a6d8312e8ec2193c49706a
DigiDuncan <digiduncan@gmail.com>
2026-08-14 05:00:45 -0400

    hover overs

--- superttt/main.py
+++ superttt/main.py
@@ -1,9 +1,10 @@
 import random
 
 from arcade import Camera2D, Sprite, SpriteList, Text, View, XYWH, LBWH, draw_rect_filled, draw_texture_rect, Rect, LRBT
+from arcade.color import WHITE
 import arcade.key
 
-from pyglet.graphics import Batch
+from pyglet.graphics import Batch, Group
 
 from superttt.game import board
 from superttt.game.board import State, Tile
@@ -171,9 +172,16 @@ class StartView(View):
         with open("./resources/superttt/splashes.txt") as f:
             self.splash_text.text = random.choice(f.readlines()).strip()
 
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> bool | None:
+        for button in [self.new_game, self.quit]:
+            if (x, y) in button.rect:
+                button.color = arcade.color.CYAN
+            else:
+                button.color = arcade.color.WHITE
+
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> bool | None:
         if (x, y) in self.new_game.rect:
-            nav.push(SettingsView())
+            nav.push(ModeView())
         elif (x, y) in self.quit.rect:
             arcade.close_window()
 
@@ -190,6 +198,11 @@ class StartView(View):
         self.splash_text.draw()
 
 class RectText(Text):
+    def __init__(self, text: str, x: float, y: float, color: tuple[int, int, int] | tuple[int, int, int, int] = arcade.color.WHITE, font_size: float = 12, width: int | None = None, align: str = "left", font_name: str | tuple[str, ...] = ("calibri", "arial"), bold: bool | str = False, italic: bool = False, anchor_x: str = "left", anchor_y: str = "baseline", multiline: bool = False, rotation: float = 0, batch: Batch | None = None, group: Group | None = None, z: float = 0, **kwargs):
+        super().__init__(text, x, y, color, font_size, width, align, font_name, bold, italic, anchor_x, anchor_y, multiline, rotation, batch, group, z, **kwargs)
+
+        self.hovered = False
+
     @property
     def rect(self) -> Rect:
         return LRBT(self.left, self.right, self.bottom, self.top)
@@ -209,13 +222,13 @@ class SettingsView(View):
         self.start = Sprite("./resources/superttt/start.png")
         self.start.scale = 0.5
         self.start.bottom = 10
-        self.start.left = 10
+        self.start.right = self.width - 10
         self.spritelist.append(self.start)
 
         self.back = Sprite("./resources/superttt/back.png")
         self.back.scale = 0.5
         self.back.bottom = 10
-        self.back.right = self.width - 10
+        self.back.left = 10
         self.spritelist.append(self.back)
 
         self.text_batch = Batch()
@@ -253,7 +266,7 @@ class SettingsView(View):
 
     def update_text_colors(self):
         for t in [self.grid_size_2, self.grid_size_3, self.grid_size_4, self.grid_size_5, self.depth_1, self.depth_2, self.depth_3, self.depth_4]:
-            t.color = arcade.color.WHITE
+            t.color = arcade.color.WHITE if not t.hovered else arcade.color.CYAN
 
         [self.grid_size_2, self.grid_size_3, self.grid_size_4, self.grid_size_5][self.grid_size - 2].color = arcade.color.RED
         [self.depth_1, self.depth_2, self.depth_3, self.depth_4][self.depth - 1].color = arcade.color.RED
@@ -286,6 +299,17 @@ class SettingsView(View):
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> bool | None:
         self.hovering_caution = (x, y) in self.caution.rect
+        for button in [self.start, self.back]:
+            if (x, y) in button.rect:
+                button.color = arcade.color.CYAN
+            else:
+                button.color = arcade.color.WHITE
+
+        for text in [self.depth_1, self.depth_2, self.depth_3, self.depth_4,
+                     self.grid_size_2, self.grid_size_3, self.grid_size_4, self.grid_size_5]:
+            text.hovered = (x, y) in text.rect
+
+        self.update_text_colors()
     
     def on_key_press(self, symbol: int, modifiers: int) -> bool | None:
         if symbol == arcade.key.BACKSPACE:
@@ -297,3 +321,50 @@ class SettingsView(View):
         self.text_batch.draw()
         if self.hovering_caution:
             self.caution_text.draw()
+
+class ModeView(View):
+    def __init__(self):
+        super().__init__()
+
+        self.spritelist = SpriteList()
+
+        self.couch = Sprite("./resources/superttt/couch.png")
+        self.couch.scale = 0.5
+        self.couch.center_x = self.center_x
+        self.couch.center_y = self.height * 0.75
+        self.spritelist.append(self.couch)
+
+        self.local = Sprite("./resources/superttt/local.png")
+        self.local.scale = 0.5
+        self.local.center_x = self.center_x
+        self.local.center_y = self.center_y
+        self.spritelist.append(self.local)
+
+        self.online = Sprite("./resources/superttt/online.png")
+        self.online.scale = 0.5
+        self.online.center_x = self.center_x
+        self.online.center_y = self.height * 0.25
+        self.spritelist.append(self.online)
+
+        self.back = Sprite("./resources/superttt/back.png")
+        self.back.scale = 0.5
+        self.back.bottom = 10
+        self.back.left = 10
+        self.spritelist.append(self.back)
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> bool | None:
+        for button in [self.couch, self.local, self.online, self.back]:
+            if (x, y) in button.rect:
+                button.color = arcade.color.CYAN
+            else:
+                button.color = arcade.color.WHITE
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> bool | None:
+        if (x, y) in self.couch.rect:
+            nav.push(SettingsView())
+        elif (x, y) in self.back.rect:
+            nav.pop()
+
+    def on_draw(self) -> bool | None:
+        self.clear()
+        self.spritelist.draw()
